7952f0698c59030ab85ba11c50e3bf5b1b040bec
Matija Galina <matijagalina@gmail.com>
2017-12-19 14:04:31 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,23 +3,6 @@
 let todoApp = {
   // todo items in an array
   todoItems: [],
-  // object method for displaying todo list items
-  displayTodoItems: function() {
-    // If there is no items in todo list (length === 0) notify the user
-    if (this.todoItems.length === 0) {
-      console.log("There is nothing to do!");
-    // if there is items in todo list go through the list and display the items
-    } else {
-      console.log('Stuff to do: ');
-      for (let i = 0; i < this.todoItems.length; i++) {
-        if (this.todoItems[i].completed === true) {
-          console.log("(x)", this.todoItems[i].todoDescription);  // if completed print with (x)
-        } else {
-          console.log("( )", this.todoItems[i].todoDescription);  //if not completed print with ( )
-        }
-      }
-    }
-  },
   // object method for adding new todo items
   addTodoItem: function (todoDescription) {
     this.todoItems.push({
@@ -27,23 +10,19 @@ let todoApp = {
       todoDescription: todoDescription,      // text of the todo item
       completed: false        // default value for todo item is not completed
     });
-    this.displayTodoItems();
   },
   // object method for changing the todo item
   changeTodoItem: function (index, todoDescription) {
     this.todoItems[index].todoDescription = todoDescription;
-    this.displayTodoItems();
   },
   // object method for deleting todo item
   deleteTodoItem: function (index) {
     this.todoItems.splice(index, 1);  // deletes only one item
-    this.displayTodoItems();
   },
   // object method for completing todo item
   toggleCompleted: function(index) {
     let item = this.todoItems[index];   // var in which the todo item is saved
     item.completed = !item.completed;   // flips the boolean of todo item
-    this.displayTodoItems();
   },
   toggleAllItems: function() {
     // variable which stoes number of all to do items
@@ -68,26 +47,23 @@ let todoApp = {
         this.todoItems[h].completed = true;
       }
     }
-    this.displayTodoItems();
   },
 };
 
-/*
-Object with functions which we can acces from html for onclick buttons
-Less lines and repetition of code, variables which contain elements and less event listeners
-*/
+// Functions which we can acces from html for onclick buttons
+// Less lines and repetition of code, variables which contain elements and less event listeners
 let handlerMethods = {
-  displayTodoItems: function() {
-    todoApp.displayTodoItems();
-  },
   toggleAllItems: function() {
     todoApp.toggleAllItems();
+    // update the list after every change
+    showList.displayTodoItems();
   },
   addTodoItem: function() {
     let addTodoText = document.getElementById("addTodoText");
     todoApp.addTodoItem(addTodoText.value);
     // clearing the input line for the next item
     addTodoText.value= "";
+    showList.displayTodoItems();
   },
   changeTodoItem: function() {
     let changeTodoIndex = document.getElementById("changeTodoIndex");
@@ -96,20 +72,50 @@ let handlerMethods = {
     // clearing the input line for the next item
     changeTodoIndex.value = "";
     changeTodoText.value = "";
+    showList.displayTodoItems();
   },
   deleteTodoItem: function() {
     let deleteTodoIndex = document.getElementById("deleteTodoIndex");
     todoApp.deleteTodoItem(deleteTodoIndex.valueAsNumber);
     // clearing the input line for the next item
     deleteTodoIndex.value="";
+    showList.displayTodoItems();
   },
   toggleCompleted: function() {
     let toggleCompletedIndex = document.getElementById("toggleCompletedIndex");
     todoApp.toggleCompleted(toggleCompletedIndex.valueAsNumber);
     // clearing the input line for the next item
     toggleCompletedIndex.value = "";
+    showList.displayTodoItems();
   },
   toggleAll: function() {
     todoApp.toggleAllItems();
+    showList.displayTodoItems();
+  }
+};
+
+// Object with methods for displaying items in a list
+var showList = {
+  displayTodoItems: function () {
+    var todoList = document.querySelector("ul");
+    // clears list before showing the to do items
+    todoList.innerHTML = "";
+    for(var i = 0; i < todoApp.todoItems.length; i++) {
+      var todoListItem = document.createElement("li");
+      var todo = todoApp.todoItems[i];
+      var todoItem = "";
+
+      // if completed print todoText with (x) otherwise with ( )
+      if (todo.completed === true) {
+        todoItem = "(x) " + todo.todoDescription;
+      } else {
+        todoItem = "( ) " + todo.todoDescription;
+      }
+
+      // adding the text from the todo item to the list item
+      todoListItem.textContent = todoItem;
+      // append the list item with todo description to the list element
+      todoList.appendChild(todoListItem);
+    }
   }
 };
